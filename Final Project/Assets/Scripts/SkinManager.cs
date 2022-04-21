5352c0096754cce4fdfe1c555a6246994db9daf4
lohare21 <lohare@iu.edu>
2022-04-21 01:52:50 -0400

    Working on code for picking a character out

--- Final Project/Assets/Scripts/SkinManager.cs
+++ Final Project/Assets/Scripts/SkinManager.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class SkinManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public SpriteRenderer sr;
+    public List<Sprite> skins = new List<Sprite>();
+    private int selectedSkin = 0;
+    public GameObject playerskin;
+
+    public void NextOption()
+    {
+        selectedSkin = selectedSkin + 1;
+        if (selectedSkin == skins.Count)
+        {
+            selectedSkin = 0;
+        }
+        sr.sprite = skins[selectedSkin];
+
+    }
+
     void Start()
     {
         
